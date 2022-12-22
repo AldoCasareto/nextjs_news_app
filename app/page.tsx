c51b33d6c4867ea3be77a483b28e026c37db46fc
Aldo <acasareto@gmail.com>
2022-12-22 10:03:09 +0100

    finished graphql endpoints

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { categories } from '../constants';
+import { fetchNews } from '../utils/fetchNews';
 
-const Home = () => {
+const Home = async () => {
+  const news: NewsResponse = await fetchNews(categories.join(','));
   return <div>HomePage</div>;
 };
 
--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,28 @@
+type Pagination = {
+  count: Int;
+  limit: Int;
+  offset: Int;
+  total: Int;
+};
+
+type Article = {
+  author: string | null;
+  category: string;
+  country: string;
+  description: string;
+  image: string | null;
+  language: string;
+  published_at: string;
+  source: string;
+  title: string;
+  url: string;
+};
+
+type NewsResponse = {
+  pagination: Pagination;
+  data: Article[];
+};
+
 type Category =
   | 'business'
   | 'entertainment'
